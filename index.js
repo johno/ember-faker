9ce195427e6fb0a7e69de01ebaf208bea4365df3
Paul Spieker <p.spieker@duenos.de>
2016-10-14 11:03:14 +0200

    fix: app.import is not a function

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ module.exports = {
   name: 'ember-faker',
 
   included: function included(app) {
+    this._super.included.apply(this, arguments);
+
+    // Fixes an issue if references as *dependency* in package.json, not as
+    // *devDependency*.
+    // See: https://github.com/ember-cli/ember-cli/issues/5747
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
     this.app = app;
     var addonConfig = this.app.project.config(app.env)['ember-faker'];
 
