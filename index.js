9dcd391020aefa1eb3686e8126b4521d66aa3309
Yoran Brondsema <yoran@sutori.com>
2017-06-09 14:50:20 -0400

    Remove obsolete code

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict';
-var path = require('path');
+let path = require('path');
 
 module.exports = {
   name: 'ember-faker',
 
-  included: function included(app) {
-    this._super.included.apply(this, arguments);
-
-    // Fixes an issue if references as *dependency* in package.json, not as
-    // *devDependency*.
-    // See: https://github.com/ember-cli/ember-cli/issues/5747
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
+  included(app) {
+    this._super(...arguments);
 
     this.app = app;
-    var addonConfig = this.app.project.config(app.env)['ember-faker'];
+    let addonConfig = this.app.project.config(app.env)['ember-faker'];
 
     if (app.env !== 'production' || addonConfig.enabled) {
       app.import(app.bowerDirectory + '/Faker/build/build/faker.js');
@@ -26,7 +19,7 @@ module.exports = {
     }
   },
 
-  blueprintsPath: function() {
+  blueprintsPath() {
     return path.join(__dirname, 'blueprints');
   }
 };
