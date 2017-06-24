e03de3ee3a412c33214df67aa865c6c8aec52e78
Yoran Brondsema <yoran@sutori.com>
2017-06-24 10:40:04 -0500

    Added an extra test

--- tests/unit/faker-test.js
+++ tests/unit/faker-test.js
@@ -3,9 +3,16 @@ import faker from 'faker';
 
 module('faker');
 
-test('it exists', function(assert) {
+test(`it imports the 'faker' module`, function(assert) {
   assert.ok(
     faker,
     `The module 'faker' is imported`
+  );
+});
+
+test(`it has 'faker.name'`, function(assert) {
+  assert.ok(
+    faker.name,
+    `It has 'faker.name'`
   );
 });
