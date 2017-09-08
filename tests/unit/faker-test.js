6cb986e94ec62f54b292ced2f533b0bbadd161bb
Yoran Brondsema <yoran@sutori.com>
2017-09-08 22:35:58 +0200

    Stylefix

--- tests/unit/faker-test.js
+++ tests/unit/faker-test.js
@@ -4,15 +4,9 @@ import faker from 'faker';
 module('faker');
 
 test(`it imports the 'faker' module`, function(assert) {
-  assert.ok(
-    faker,
-    `The module 'faker' is imported`
-  );
+  assert.ok(faker, `The module 'faker' is imported`);
 });
 
 test(`it has 'faker.name'`, function(assert) {
-  assert.ok(
-    faker.name,
-    `It has 'faker.name'`
-  );
+  assert.ok(faker.name, `It has 'faker.name'`);
 });
