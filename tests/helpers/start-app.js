eb85b3f1b96f3465de04f83b6e3d5fbd8204abac
Yoran Brondsema <yoran@sutori.com>
2017-10-27 19:53:04 -0500

    ember init

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,26 +1,16 @@
-import { run } from '@ember/runloop';
-import { merge } from '@ember/polyfills';
 import Application from '../../app';
-import Router from '../../router';
 import config from '../../config/environment';
+import { merge } from '@ember/polyfills';
+import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
-  var App;
-
-  var attributes = merge({}, config.APP);
+  let attributes = merge({}, config.APP);
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  Router.reopen({
-    location: 'none'
+  return run(() => {
+    let application = Application.create(attributes);
+    application.setupForTesting();
+    application.injectTestHelpers();
+    return application;
   });
-
-  run(function() {
-    App = Application.create(attributes);
-    App.setupForTesting();
-    App.injectTestHelpers();
-  });
-
-  App.reset(); // this shouldn't be needed, i want to be able to "start an app at a specific URL"
-
-  return App;
 }
